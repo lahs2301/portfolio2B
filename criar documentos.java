9e217440450c2d83647f647f0055198c0885c8c6
lahs2301 <102532494+lahs2301@users.noreply.github.com>
2022-04-11 10:59:08 -0300

    Criei alguns documentos e arquivos e movi eles

---
+++ criar documentos.java
@@ -0,0 +1,34 @@
+1.
+  mkdir pallet
+  cd pallet
+  mkdir ash gary oaskLAB
+  cd ash
+  mkdir pokedex
+  touch bulbassauro.txt butterfree.txt charmander.txt
+  cd pokedex
+  touch zapdos.txt hoothoot.txt
+  cd ..
+  cd ..
+  cd gary
+  touch blastoise.jpg intro.mp3 eevee.jpg
+  cd..
+  cd ..
+  cd oaksLAB
+  cd oaskLAB
+  touch pokedex1996.exe pokedex2014.exe
+  cd ..
+  cd ..
+  history
+
+2.
+  cd pallet
+  cd ash
+  cp b* ../oaskLAB
+
+3.
+ cd ..
+ cd oaskLAB
+ cp ../ash/charmander.txt .
+
+4.
+touch ../ash/snorlax.txt ../ash/totodile.txt
